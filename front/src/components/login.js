d250e55e82114094b03d2acbb104febf9f4ea257
Jakub Róg <jakubrog25@gmail.com>
2020-04-04 13:21:54 +0200

    LZ-14 handling events

--- front/src/components/login.js
+++ front/src/components/login.js
@@ -1,20 +1,47 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            email: props.email? props.email : '',
+            password: props.password? props.password : '',
+            error: false
+        }
+        this.login = this.login.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        if (target.name === "password") {
+            this.setState({password: event.target.value});
+        }else{
+            this.setState({email: event.target.value})
+        }
+    }
+
+    login(event){
+        alert(this.state.email + "\n"
+            + this.state.password
+        );
+
+        event.preventDefault();
+    }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.login}>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input name="email" onChange={this.handleChange} type="email" className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input name="password" onChange={this.handleChange} type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
@@ -23,8 +50,7 @@ export default class Login extends Component {
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <button className="btn btn-primary btn-block">Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
--- front/src/components/signup.js
+++ front/src/components/signup.js
@@ -1,29 +1,69 @@
 import React, { Component } from "react";
 
 export default class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: ''
+        }
+        this.signUp = this.signUp.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        switch(target.name){
+            case "firstName":
+                this.setState({firstName: event.target.value})
+                break;
+            case "lastName":
+                this.setState({lastName: event.target.value})
+                break;
+            case "email":
+                this.setState({email: event.target.value})
+                break;
+            case "password":
+                this.setState({password: event.target.value})
+                break;
+        }
+    }
+
+    signUp(event){
+        alert(this.state.firstName + "\n"
+            + this.state.lastName + "\n"
+            + this.state.email + "\n"
+            + this.state.password
+        );
+
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.signUp}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input name="firstName" onChange={this.handleChange} type="text" className="form-control" placeholder="First name" />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input name="lastName" onChange={this.handleChange} type="text" className="form-control" placeholder="Last name" />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input name="email"  onChange={this.handleChange} type="email" className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input name="password" onChange={this.handleChange} type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
