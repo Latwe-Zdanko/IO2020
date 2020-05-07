55aed354c42cf52e3f9003d0d2f774981043d218
= <=>
2020-05-07 14:17:14 +0200

    LZ-30 prototype

--- front/src/components/signup.js
+++ front/src/components/signup.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 import AuthenticationService from "../service/AuthenticationService";
 import {Redirect} from "react-router-dom";
+import axios from 'axios';
+import bcryptjs from 'bcryptjs'
+
+const API_URL = 'http://localhost:8080';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -9,8 +13,10 @@ export default class SignUp extends Component {
             firstName: '',
             lastName: '',
             email: '',
-            password: ''
+            password: '',
+            hashed: false
         };
+
         this.signUp = this.signUp.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
@@ -18,16 +24,33 @@ export default class SignUp extends Component {
     handleChange(event) {
         const {name, value} = event.target;
         this.setState({[name]: value});
+        if(name === "password"){
+            this.state.hashed = false;
+        }
     }
 
     signUp(event) {
-        alert(this.state.firstName + "\n"
-            + this.state.lastName + "\n"
-            + this.state.email + "\n"
-            + this.state.password
-        );
-
         event.preventDefault();
+        const url = API_URL + "/add-user";
+        this.hashPassword();
+        axios.get(url, {params: this.state})
+            .then(r => {
+                this.redirect();
+                this.props.history.push("/");
+            }).catch(r => alert(r))
+    }
+
+    redirect(){
+        let form = document.getElementById("sign-form");
+        form.hidden = true;
+        let cont = document.getElementById("main-cont");
+    }
+
+    hashPassword() {
+        if(!this.state.hashed) {
+            this.state.password = bcryptjs.hashSync(this.state.password, 10);
+            this.state.hashed = true;
+        }
     }
 
     render() {
@@ -36,32 +59,31 @@ export default class SignUp extends Component {
         }
         return (
             <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <form onSubmit={this.signUp}>
+                <div id="main-cont" className="auth-inner">
+                    <form id="sign-form" onSubmit={this.signUp}>
                         <h3>Sign Up</h3>
-
                         <div className="form-group">
                             <label>First name</label>
                             <input name="firstName" onChange={this.handleChange} type="text" className="form-control"
-                                   placeholder="First name"/>
+                                   placeholder="First name" required="required"/>
                         </div>
 
                         <div className="form-group">
                             <label>Last name</label>
                             <input name="lastName" onChange={this.handleChange} type="text" className="form-control"
-                                   placeholder="Last name"/>
+                                   placeholder="Last name" required="required"/>
                         </div>
 
                         <div className="form-group">
                             <label>Email address</label>
                             <input name="email" onChange={this.handleChange} type="email" className="form-control"
-                                   placeholder="Enter email"/>
+                                   placeholder="Enter email" required="required"/>
                         </div>
 
                         <div className="form-group">
                             <label>Password</label>
                             <input name="password" onChange={this.handleChange} type="password" className="form-control"
-                                   placeholder="Enter password"/>
+                                   placeholder="Enter password" required="required"/>
                         </div>
 
                         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
