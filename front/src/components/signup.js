ec5e7e8d4052e4eabff4724fd398e3aea0bb1db7
= <=>
2020-05-09 14:08:57 +0200

    LZ30 - added: checking fields, redirect

--- front/src/components/signup.js
+++ front/src/components/signup.js
@@ -3,10 +3,11 @@ import AuthenticationService from "../service/AuthenticationService";
 import {Redirect} from "react-router-dom";
 import axios from 'axios';
 import bcryptjs from 'bcryptjs'
-
+import ReactDOM from "react-dom"
 const API_URL = 'http://localhost:8080';
 
 export default class SignUp extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -32,18 +33,36 @@ export default class SignUp extends Component {
     signUp(event) {
         event.preventDefault();
         const url = API_URL + "/add-user";
-        this.hashPassword();
-        axios.get(url, {params: this.state})
-            .then(r => {
-                this.redirect();
-                this.props.history.push("/");
-            }).catch(r => alert(r))
+        if(this.checkFields()){
+            this.hashPassword();
+            axios.get(url, {params: this.state})
+                .then(() => this.redirectIn(4))
+                .catch(() => this.alreadyRegistered())
+        }
     }
 
-    redirect(){
-        let form = document.getElementById("sign-form");
-        form.hidden = true;
-        let cont = document.getElementById("main-cont");
+    redirectIn(time){
+        const element = <h2><p>Successful register!</p><p>Redirect in {time}!</p></h2>;
+        ReactDOM.render(element, document.getElementById('main-cont'));
+        time--;
+        let x = setInterval(() =>{
+            if(time <= 0){
+                this.props.history.push("/");
+                clearInterval(x);
+                return;
+            }
+            const element = <h2><p>Successful register!</p><p>Redirect in {time}!</p></h2>;
+            ReactDOM.render(element, document.getElementById('main-cont'));
+            time--;
+        },1000);
+    }
+
+    alreadyRegistered(){
+        document.getElementById("email").style.borderColor = "red";
+        let label = document.getElementById("email-label");
+        label.style.color = "red";
+        label.innerText += " - already registered";
+
     }
 
     hashPassword() {
@@ -51,6 +70,17 @@ export default class SignUp extends Component {
             this.state.password = bcryptjs.hashSync(this.state.password, 10);
             this.state.hashed = true;
         }
+    }
+
+    checkFields() {
+        if(this.state.password.length < 8){
+            document.getElementById("password").style.borderColor = "red";
+            let label = document.getElementById("password-label");
+            label.style.color = "red";
+            label.innerText += " length must be >= 8";
+            return false;
+        }
+        return true;
     }
 
     render() {
@@ -75,17 +105,16 @@ export default class SignUp extends Component {
                         </div>
 
                         <div className="form-group">
-                            <label>Email address</label>
-                            <input name="email" onChange={this.handleChange} type="email" className="form-control"
+                            <label id="email-label"> Email address</label>
+                            <input id="email" name="email" onChange={this.handleChange} type="email" className="form-control"
                                    placeholder="Enter email" required="required"/>
                         </div>
 
                         <div className="form-group">
-                            <label>Password</label>
-                            <input name="password" onChange={this.handleChange} type="password" className="form-control"
+                            <label id="password-label">Password</label>
+                            <input id="password" name="password" onChange={this.handleChange} type="password" className="form-control"
                                    placeholder="Enter password" required="required"/>
                         </div>
-
                         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                         <p className="forgot-password text-right">
                             Already registered <a href="/sign-in">sign in?</a>
@@ -95,4 +124,6 @@ export default class SignUp extends Component {
             </div>
         );
     }
+
+
 }
