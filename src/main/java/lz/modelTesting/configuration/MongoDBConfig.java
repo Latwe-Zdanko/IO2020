36688f74a4e4d3e4ff3f96b3b95ef4f662f9daef
mimagiera <michalmagierra@gmail.com>
2020-05-28 20:53:52 +0200

    [LZ-31] little sample generation refactor

--- src/main/java/lz/modelTesting/configuration/MongoDBConfig.java
+++ src/main/java/lz/modelTesting/configuration/MongoDBConfig.java
@@ -19,14 +19,14 @@ import java.util.Optional;
 public class MongoDBConfig {
     @Bean
     CommandLineRunner loadSamples(UsersRepository usersRepository, MockupsRepository mockupsRepository,
-                                 SurveysRepository surveysRepository, ProjectsRepository projectsRepository) {
+                                  ProjectsRepository projectsRepository) {
         return args ->
         {
             // passwords are encrypted by BCrypt
             Optional<User> userOptional = usersRepository.findByEmail("u1@mail.pl");
             if (userOptional.isEmpty()) {
                 // password: passuser1
-                usersRepository.save(new User("First 1","Last 1", "u1@mail.pl", "$2y$12$CvynPUYvZqt4p3aiuGaGduG5RU5rIsKDJ2BbcYk7d1nTDAZGP2Woe"));
+                usersRepository.save(new User("First 1", "Last 1", "u1@mail.pl", "$2y$12$CvynPUYvZqt4p3aiuGaGduG5RU5rIsKDJ2BbcYk7d1nTDAZGP2Woe"));
             }
             userOptional = usersRepository.findByEmail("u2@mail.pl");
             if (userOptional.isEmpty()) {
@@ -34,12 +34,22 @@ public class MongoDBConfig {
                 usersRepository.save(new User("First 2 ", "Last 2", "u2@mail.pl", "$2y$12$xswpjLRaupJE8y4CHHIbjeba9BvlV.VLfut8q1Gqj5U2JDHgjNmuG"));
             }
 
-            Project project = new Project("project 1");
-            projectsRepository.save(project);
-            mockupsRepository.save(new Mockup("Axure", "https://2usnmc.axshare.com/",project.getId()));
-            mockupsRepository.save(new Mockup("AdobeXD", "https://xd.adobe.com/embed/e6a0d97b-6bfc-4f07-653f-70a6a2eae5a7-9091/",project.getId()));
-            mockupsRepository.save(new Mockup("Figma", "https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/a32Lpn3oXSef2HgPtu5BQx/Course-Dashboard-Copy?node-id=1%3A10&scaling=scale-down-width",project.getId()));
-
+            saveSampleProject(mockupsRepository, projectsRepository);
         };
     }
+
+    private void saveSampleProject(MockupsRepository mockupsRepository, ProjectsRepository projectsRepository) {
+        String sampleProject = "Sample project";
+        Optional<Project> projectOptional = projectsRepository.findByName(sampleProject);
+        if (projectOptional.isEmpty()) {
+            Project project = new Project(sampleProject);
+            projectsRepository.save(project);
+            String projectId = project.getId();
+            mockupsRepository.save(new Mockup("Axure", "https://2usnmc.axshare.com/", projectId));
+            mockupsRepository.save(new Mockup("AdobeXD", "https://xd.adobe.com/embed/e6a0d97b-6bfc-4f07-653f-70a6a2eae5a7-9091/", projectId));
+            mockupsRepository.save(new Mockup("Figma", "https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/a32Lpn3oXSef2HgPtu5BQx/Course-Dashboard-Copy?node-id=1%3A10&scaling=scale-down-width", projectId));
+        }
+    }
+
+
 }
