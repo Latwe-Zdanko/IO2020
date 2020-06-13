749963d863cd02c606c29bcd35a2d0a9bf296bec
osman <osa123125@o2.pl>
2020-06-13 12:17:53 +0200

    M3-LZ-41 Wizualizacja danych

--- front/src/components/ViewData.js
+++ front/src/components/ViewData.js
@@ -3,6 +3,7 @@ import "../App.css";
 import "survey-react/survey.css";
 import axios from 'axios';
 import AuthenticationService from "../service/AuthenticationService";
+import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis,} from 'recharts';
 
 class ViewData extends Component {
 
@@ -11,13 +12,9 @@ class ViewData extends Component {
         this.state = {
             survey: {},
             answers: [],
-            serverUrl: process.env.REACT_APP_SERVER_URL
+            serverUrl: process.env.REACT_APP_SERVER_URL,
         };
 
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
-
-    componentDidMount() {
         const headers = {headers: {authentication: AuthenticationService.getAuthToken()}};
 
         axios.get(this.state.serverUrl + '/surveys/' + this.props.match.params.id, headers)
@@ -32,17 +29,72 @@ class ViewData extends Component {
                     answers: answers,
                     survey: JSON.parse(data.body)
                 });
+                this.getDataFromJson()
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
 
+    createChart = (title, answers, output) => {
+        let counts = {};
+        let data = [];
+        answers[1].forEach(function (x) {
+            counts[x] = (counts[x] || 0) + 1;
+        });
+        answers[2].forEach((option) => {
+            data.push({name: option, answers: counts[option]});
+        });
+        output.push(
+            <div className="chart-div">
+                <h3>{title}</h3>
+                <BarChart className="chart"
+                          width={600}
+                          height={300}
+                          data={data}
+                          margin={{
+                              top: 5, right: 30, left: 20, bottom: 5,
+                          }}
+                          barSize={40}
+                >
+                    <XAxis dataKey="name" scale="point" padding={{left: 20, right: 10}}/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Legend/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Bar dataKey="answers" fill="#1ab394" background={{fill: '#eee'}}/>
+                </BarChart>
+            </div>
+        )
+    };
+
+    createAnswerList = (question, output) => {
+
+        let answers = [];
+        question.questions[1].forEach((answer) => {
+            answers.push(
+                <div className="answer">
+                    <p>{answer}</p>
+                </div>
+            )
+        });
+        output.push(
+            <div className="answer-list-div">
+                <h2 className="answer-list-title">{question.title}</h2>
+                <div className="answer-list">
+                    {answers}
+                </div>
+            </div>
+        )
+    };
+
     getDataFromJson = () => {
         const questions = this.state.survey.questions;
-        if (questions === undefined) return;
 
-        console.log(questions, this.state.answers);
+        if (questions === undefined) return;
+        this.setState({
+            questions: questions
+        });
         const answers = [];
         let results = [];
         questions.map((question) => {
@@ -53,14 +105,20 @@ class ViewData extends Component {
                     answer["name"] = question.name;
                     answer["type"] = question.type;
                     answer["questions"] = [];
+                    let choices = [];
+                    question.columns.map((col) => {
+                        choices.push(col.text);
+                    });
                     question.rows.map((row) => {
                         results = [];
-                        this.state.answers.map((ans) => {
-                            if (ans[question.name] !== undefined) {
-                                results.push(ans[question.name][row.value]);
-                            }
-                        });
-                        answer["questions"].push([row.value, results]);
+                        if (row.text !== "") {
+                            this.state.answers.map((ans) => {
+                                if (ans[question.name] !== undefined) {
+                                    results.push(choices[ans[question.name][row.value]]);
+                                }
+                            });
+                            answer["questions"].push([row.value, results, choices]);
+                        }
                     });
 
                     answers.push(answer);
@@ -72,12 +130,13 @@ class ViewData extends Component {
                     answer["min"] = question.mininumRateDescription;
                     answer["max"] = question.maximumRateDescription;
                     results = [];
+
                     this.state.answers.map((ans) => {
                         if (ans[question.name] !== undefined) {
                             results.push(ans[question.name]);
                         }
                     });
-                    answer["questions"] = [question.title, results];
+                    answer["questions"] = [question.title, results, [1, 2, 3, 4, 5]];
                     answers.push(answer);
                     break;
                 case "comment":
@@ -104,20 +163,55 @@ class ViewData extends Component {
                             results.push(ans[question.name]);
                         }
                     });
-                    answer["questions"] = [question.title, results];
+                    answer["questions"] = [question.title, results, question.choices];
                     answers.push(answer);
                     break;
             }
         });
+
+        this.setState({
+            answers: answers
+        });
+    };
+
+    displayAnswers = (answers) => {
+        let output = [];
         console.log(answers);
+        answers.forEach((answer) => {
+            switch (answer.type) {
+                case "radiogroup":
+                    this.createChart(answer.title, answer.questions, output);
+                    break;
+                case "rating":
+                    this.createChart(answer.title, answer.questions, output);
+                    break;
+                case "matrix":
+                    output.push(<h2>{answer.title}</h2>);
+                    answer.questions.forEach((question) => {
+                        this.createChart(question[0], question, output);
+                    });
+                    break;
+                case "comment":
+                    this.createAnswerList(answer, output);
+
+            }
+        });
+        return output;
     };
 
     render() {
-        return (
-            <div className="wrapper">
-                <div>
-                    {this.getDataFromJson()}
+
+        const displayAnswers = (this.state.questions !== undefined) ? (
+            (
+                <div style={{"background-color": "white"}}>
+                    {this.displayAnswers(this.state.answers)}
                 </div>
+            )
+        ) : null;
+
+        return (
+            <div className="wrapper" style={{"background-color": "white"}}>
+                {displayAnswers}
             </div>
         );
     }
